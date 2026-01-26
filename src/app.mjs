44e71ece37ec60a1d34b86a07c134747b9498e9e
BLuBin7 <builuongbinh7@gmail.com>
2026-01-26 17:19:32 +0700

    refactor: switch to external hosting library for express server

--- src/app.mjs
+++ src/app.mjs
@@ -1,13 +1,29 @@
+// DEV
+// /**
+//  * Application Entry Point
+//  */
+// import { JiraBot } from './bot/JiraBot.mjs'
+// import { validateConfig } from './config/env.mjs'
+// import { createServer, startServer } from './server.mjs'
+
+// validateConfig()
+
+// const bot = new JiraBot()
+// const app = createServer(bot)
+
+// startServer(app)
+
+
+// PROD
 /**
  * Application Entry Point
  */
 import { JiraBot } from './bot/JiraBot.mjs'
 import { validateConfig } from './config/env.mjs'
-import { createServer, startServer } from './server.mjs'
+import { startServer } from '@microsoft/agents-hosting-express'                                                                                       
 
 validateConfig()
 
 const bot = new JiraBot()
-const app = createServer(bot)
 
-startServer(app)
+startServer(bot)
